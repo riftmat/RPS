39242c048781b94bfaf74c1722b86e52dd93049c
Patryk Lachowicz <118370365+riftmat@users.noreply.github.com>
2025-02-21 16:47:31 +0100

    Add files via upload

--- papier_kamień.py
+++ papier_kamień.py
@@ -3,14 +3,30 @@ import random
 
 emoji = {'r': '🪨', 's': '✂️', 'p': '🧻'}
 choices = ('r', 'p', 's')
-pick = input('Rock, paper, scisors (r/p/s): ').lower()
 
-if pick not in choices:
-    print('Invalid choice')
+while True:
+    pick = input('Rock, paper, scisors (r/p/s): ').lower()
 
-ai_pick = random.choice(choices)
+    if pick not in choices:
+        print('Invalid choice')
+        continue
+
+    ai_pick = random.choice(choices)
 
 
-print(f'you chose {emoji[pick]}')
-print(f'computer chose {emoji[ai_pick]}')
+    print(f'you chose {emoji[pick]}')
+    print(f'computer chose {emoji[ai_pick]}')
 
+    if pick == ai_pick:
+        print('Draw')
+    elif (
+        (pick == 'r' and ai_pick == 's') or 
+        (pick == 's' and ai_pick == 'p') or 
+        (pick == 'p' and ai_pick == 'r')):
+        print('you win')
+    else:
+        print('you lose')
+
+    next = input('Continue? y/n: ').lower()
+    if next == 'n':
+        break
