c21205f9fa0cdb88e02f34074699a8b4a34b50d3
Patryk Lachowicz <118370365+riftmat@users.noreply.github.com>
2025-02-24 15:26:07 +0100

    Add files via upload

--- papier_kamień.py
+++ papier_kamień.py
@@ -4,21 +4,21 @@ import random
 emoji = {'r': '🪨', 's': '✂️', 'p': '🧻'}
 choices = ('r', 'p', 's')
 
-while True:
-    pick = input('Rock, paper, scisors (r/p/s): ').lower()
-
-    if pick not in choices:
-        print('Invalid choice')
-        continue
-
-    ai_pick = random.choice(choices)
-
-
+def get_user_choice():
+    while True:
+        pick = input('Rock, paper, scisors (r/p/s): ').lower()
+        if pick in choices:
+            return pick
+        else:
+            print('invalid choice')
+            
+def show_pick(pick, ai_pick):
     print(f'you chose {emoji[pick]}')
     print(f'computer chose {emoji[ai_pick]}')
 
+def win_conditions(pick, ai_pick):
     if pick == ai_pick:
-        print('Draw')
+        print('Tie')
     elif (
         (pick == 'r' and ai_pick == 's') or 
         (pick == 's' and ai_pick == 'p') or 
@@ -27,6 +27,18 @@ while True:
     else:
         print('you lose')
 
-    next = input('Continue? y/n: ').lower()
-    if next == 'n':
-        break
+def play_game():
+    while True:
+        pick = get_user_choice()
+        
+        ai_pick = random.choice(choices)
+
+        show_pick(pick, ai_pick)
+
+        win_conditions(pick, ai_pick)
+
+        next = input('Continue? y/n: ').lower()
+        if next == 'n':
+            break
+
+play_game()
